d91a59608d774b23db7416d14bb289ca78ed24b6
Jensevent <j.heesb1@gmail.com>
2022-05-09 14:13:27 +0200

    Added backend project

---
+++ Musika.Backend/Audio.Microservice.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Audio.Microservice.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Musika.Backend/Chords.Microservice.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Chords.Microservice.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
